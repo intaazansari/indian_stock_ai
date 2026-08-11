9c7d14dcec3151eeff6a6045563ec2221e92b6b9
insari <intaaz.ansari@volvocars.com>
2026-08-11 16:05:07 +0200

    feat: add Google Analytics (G-J62F8KGPEJ)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { QueryProvider } from "@/components/providers/QueryProvider";
 import { BackendWakeupBanner } from "@/components/providers/BackendWakeupBanner";
 import "./globals.css";
@@ -42,6 +43,7 @@ export default function RootLayout({
             {children}
           </QueryProvider>
         </ThemeProvider>
+        <GoogleAnalytics gaId="G-J62F8KGPEJ" />
       </body>
     </html>
   );
